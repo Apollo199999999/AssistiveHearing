d219a78018dc3957e8fc89b23b3688f71269c939
Matthias Wang <matthiaswang60@gmail.com>
2024-08-20 11:42:20 +0800

    Buzzer test

---
+++ ESP32SIC/BuzzerTest/BuzzerTest.ino
@@ -0,0 +1,19 @@
+const int buzzer = 17;  // Buzzer pin
+
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+}
+ 
+void loop()
+{
+  digitalWrite(buzzer, HIGH);
+  tone(buzzer, 1000);
+  delay(5000);
+  noTone(buzzer);
+  digitalWrite(buzzer, LOW);
+}
