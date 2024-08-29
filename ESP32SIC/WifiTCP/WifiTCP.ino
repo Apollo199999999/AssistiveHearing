4bc9108661ae3f7b879678cef632c1c206d33bf0
Matthias Wang <matthiaswang60@gmail.com>
2024-08-29 09:50:30 +0800

    Update WifiTCP.ino

--- ESP32SIC/WifiTCP/WifiTCP.ino
+++ ESP32SIC/WifiTCP/WifiTCP.ino
@@ -13,45 +13,6 @@
 const char* ssid     = "ESP32-AP";
 const uint16_t portNumber = 50000; // System Ports 0-1023, User Ports 1024-49151, dynamic and/or Private Ports 49152-65535
 
-// For writing mic data to an audio buffer
-#define AUDIO_BUFFER_MAX 3200
-
-uint8_t audioBuffer[AUDIO_BUFFER_MAX];
-uint8_t transmitBuffer[AUDIO_BUFFER_MAX];
-uint32_t bufferPointer = 0;
-
-// Whether to transmit the audio data via TCP
-bool transmitNow = false;
-
-hw_timer_t * micTimer = NULL; // our timer
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED; 
-
-void IRAM_ATTR getMicSamples() {
-  portENTER_CRITICAL_ISR(&timerMux); // says that we want to run critical code and don't want to be interrupted
-  int adcVal7 = adc1_get_raw(ADC1_CHANNEL_7); // reads the ADC
-  int adcVal6 = adc1_get_raw(ADC1_CHANNEL_6); // reads the ADC
-
-  uint16_t value6 = map(adcVal6, 0 , 4096, 0, 65535);  // converts the value to 0..65535 (16bit)
-  audioBuffer[bufferPointer] = value6 & 0xff; // because we can only transmit byte arrays via TCP, we need to split the 16 bit number into 2 bytes
-  bufferPointer++;
-  audioBuffer[bufferPointer] = (value6 >> 8); 
-  bufferPointer++;
-  
-  uint16_t value7 = map(adcVal7, 0 , 4096, 0, 65535);  // converts the value to 0..65535 (16bit)
-  audioBuffer[bufferPointer] = value7 & 0xff; // because we can only transmit byte arrays via TCP, we need to split the 16 bit number into 2 bytes
-  bufferPointer++;
-  audioBuffer[bufferPointer] = (value7 >> 8); 
-  bufferPointer++;
- 
-  if (bufferPointer == AUDIO_BUFFER_MAX) { // when the buffer is full
-    bufferPointer = 0;
-    memcpy(transmitBuffer, audioBuffer, AUDIO_BUFFER_MAX); // copy buffer into a second buffer
-    transmitNow = true; // sets the value true so we know that we can transmit the audio buffer via TCP
-  }
-  portEXIT_CRITICAL_ISR(&timerMux); // says that we have run our critical code
-}
-
-
 // Configure the AP for this ESP32
 WiFiServer server(portNumber);
 WiFiClient client;
@@ -79,27 +40,6 @@ void setup() {
   Serial.print("TCP-Server on port ");
   Serial.print(portNumber);
   Serial.print(" started");
-
-  adc1_config_width(ADC_WIDTH_12Bit); // configure the analogue to digital converter
-  adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_11db); // connects the ADC 1 with channel 7 (GPIO 35)
-  adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_11db); // connects the ADC 1 with channel 6 (GPIO 35)
-
-  // We create a timer to gather audio samples from the microphone via ADC
-  // In an ESP32, there are 4 hardware timers in total, denoted by an ID from 0 to 3, that we pass in timerBegin.
-  // Each timer runs at 80MHz by default, that we can divide by a prescaler value to lower its rate
-  // In this case, we are creating a timer that runs at a rate 80MHz/80 = 1 MHz.
-  // This timer will then count upwards starting from 0, every 1 microsecond.
-  micTimer = timerBegin(0, 80, true);
-
-  // To actually get the timer to execute anything, we can get the timer to execute a function when it is interrupted.
-  timerAttachInterrupt(micTimer, &getMicSamples, true); // binds the handling function to our timer 
-
-  // We configure when we want the timer to interrupt, so that it can get audio samples.
-  // In this case, it interrupts when its counter reaches 125, giving us a sample rate of 10^6 / 125 = 8000Hz
-  timerAlarmWrite(micTimer, 125, true);
-
-  // Start the timer
-  timerAlarmEnable(micTimer);
 
   secondTimer.STOP;
 }
@@ -144,12 +84,6 @@ void loop() {
           i = 0;
         }
     }  
-
-    if (transmitNow) { // checks if the audio buffer is full
-      transmitNow = false;
-      client.write((const uint8_t *)audioBuffer, sizeof(audioBuffer)); // sending the buffer to our server
-      Serial.println("audio transmit");
-    }
   }
   else {
     Serial.println("Client has disconnected from TCP.");
